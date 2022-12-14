776fdd2198f1c08390916de77a7c82713971f20b
han-seohyun <000312@daum.net>
2022-12-14 12:59:49 +0900

    study : move page by Link tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import propTypes from 'prop-types'
-const Card = ({ title, children }) => {
+const Card = ({ title, children, onClick }) => {
   //자식 컴포넌트에서 props 받기
 
   //(props) => 객체로 props 가 전달됨 : {title : 'title'} 형태 / props.title 로 꺼내 쓰면 됨
@@ -8,7 +8,9 @@ const Card = ({ title, children }) => {
   //children : 부모 컴포넌트의 Card 태그 사이에 넣은 객체들이 전달됨
 
   return (
-    <div className="card mb-3">
+    <div 
+    className="card mb-3"
+    onClick = {onClick}> 
       <div className="card-body">
         <div className="d-flex justify-content-between">
           <div>{title}</div>
@@ -21,11 +23,15 @@ const Card = ({ title, children }) => {
 
 Card.propTypes = { //정해진 type 외 다른 type 이 들어왔을 경우 console 을 통해 warning
     title : propTypes.string,
+    children : propTypes.element,
+    onClick : propTypes.func, //Card 컴포넌트에서만 onClick 이벤트 가능
 };
+
 
 //props 의 default 값을 설정할 수 있음
 Card.defaultProps = {
-    title : 'title'
+    title : 'title',
+    onClick: ()=> {}
 };
 
 //{children && <div>buttons</div>} : children 이 있을 경우에 보여줌
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -21,19 +21,26 @@ const ListPage = () => {
     <div>
       <div className="d-flex justify-content-between"> 
         <h1>Blogs</h1>
-        <Link to="/blogs/create" className="btn btn-success mb-2"> Create New 
+        <Link 
+        to="/blogs/create" 
+        className="btn btn-success mb-2"
+        > 
+        Create New 
         </Link>
       </div>
       {posts.map((post) => {
-        //post 데이터는 ListPage 에서는 확인가능하지만,
+        //post 데이터는 ListPage 에서는 확인가능 하지만,
         //자식 컴포넌트 Card 는 확인할 수 없다. 따라서 props 를 사용 !
         //map loop
         return (
-          <Card key={post.id}>
-            <div>hi</div>
-          </Card>
+          <Card 
+          key={post.id} 
+          titile={post.titile} 
+          onClick={()=>{console.log('클릭')}}
+          />
           //props : 자식 컴포넌트에 넘기고싶은 이름은 속성으로 적고, 넘길 데이터를 {} 안에 넣어준다.
-        );
+          //Card 태그에 직접 onClick 을 사용할 수 없다. 직접 CArd 컴포넌트에 들어가서 이벤트를 걸어줘야 함 (그래서 props 로 전달)
+          );
       })}
     </div>
   );
